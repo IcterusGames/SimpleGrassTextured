123307cf7cb83a785535b0c60e9c91212050f8dc
jacob <jacob@samochud.home>
2024-01-09 19:25:53 +0100

    Duplicate default mesh on load to stop MultiMeshInstance3D from overwriting the file

--- addons/simplegrasstextured/grass.gd
+++ addons/simplegrasstextured/grass.gd
@@ -77,7 +77,7 @@ var wind_strength := 0.15 : set = _on_set_wind_strength
 var wind_turbulence := 1.0 : set = _on_set_wind_turbulence
 var wind_pattern : Texture = null : set = _on_set_wind_pattern
 
-var _default_mesh : Mesh = load("res://addons/simplegrasstextured/default_mesh.tres")
+var _default_mesh : Mesh = load("res://addons/simplegrasstextured/default_mesh.tres").duplicate()
 var _buffer_add : Array[Transform3D] = []
 var _material := load("res://addons/simplegrasstextured/materials/grass.material").duplicate() as ShaderMaterial
 var _force_update_multimesh := false
